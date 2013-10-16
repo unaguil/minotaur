aeb9aea35d288b71c67e42cbda1f20e0b5bfeea1
Unai Aguilera <unai.aguilera@deusto.es>
2013-10-16 16:40:59 +0200

    Non comma separated names also matched

--- person_statistics.py
+++ person_statistics.py
@@ -10,27 +10,15 @@ cursor = conn.cursor()
 cursor.execute('select count(*) from person;')
 total_persons = cursor.fetchall()[0][0]
 
-#cursor.execute('select count(distinct name) from person;')
-#total_distinct_persons = cursor.fetchall()[0][0]
-
-regex = '^([[:space:]]*((de(l?|([[:space:]]+la))|san|la)[[:space:]])?[[:alpha:]-]+){1,2}[[:space:]]*,[[:space:]]+[[:alpha:][:space:]\.-]+$'
-
-cursor.execute("select count(*) from person where name like '%,%';")
-total_persons_with_comma = cursor.fetchall()[0][0]
+regex = '^([[:space:]]*((de(l?|([[:space:]]+la))|san|la)[[:space:]])?[[:alpha:]-]+){1,2}[[:space:]]*,?[[:space:]]+[[:alpha:][:space:]\.-]+$'
 
 cursor.execute("select count(*) from person where lower(name) regexp '%s';" % regex)
-total_persons_matched = cursor.fetchall()[0][0]
+persons_matched = cursor.fetchall()[0][0]
 
-#print 'Distinct persons: %.2f' % (total_distinct_persons / float(total_persons) * 100)
+cursor.execute("select count(*) from person where lower(name) not regexp '%s';" % regex)
+missing_persons = cursor.fetchall()[0][0]
 
-cursor.execute("select count(*) from person where name like '%%,%%' and lower(name) not regexp '%s';" % regex)
-total_missing_persons = cursor.fetchall()[0][0]
+print "select name from person where lower(name) not regexp '%s' limit 10;" % regex
 
-print "select name from person where name like '%%,%%' and lower(name) not regexp '%s' limit 10;" % regex
-
-print 'Persons with comma: %.2f' % (total_persons_with_comma / float(total_persons) * 100)
-print '\t Matching: %.2f' % (total_persons_matched / float(total_persons_with_comma) * 100)
-print '\t Missing: %.2f' % (total_missing_persons / float(total_persons_with_comma) * 100)
-
-print ''
-print 'Total missing: %.2f' % ((total_persons - total_persons_with_comma + total_missing_persons) / float(total_persons) * 100)
+print 'Matching: %.2f' % (persons_matched / float(total_persons) * 100)
+print 'Missing: %.2f' % (missing_persons / float(total_persons) * 100)
