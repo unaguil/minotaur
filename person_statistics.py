cb7f48e39e4ce44c408364e4bac7611583f6a708
Unai Aguilera <unai.aguilera@deusto.es>
2013-10-16 16:01:23 +0200

    More surnames matched: 'san ***'. Problem with spaces before comma solved

--- person_statistics.py
+++ person_statistics.py
@@ -13,7 +13,7 @@ total_persons = cursor.fetchall()[0][0]
 #cursor.execute('select count(distinct name) from person;')
 #total_distinct_persons = cursor.fetchall()[0][0]
 
-regex = '^(de(l?|([[:space:]]+la))[[:space:]])?[[:alpha:]-]+([[:space:]]+(de(l?|([[:space:]]+la))[[:space:]])?[[:alpha:]-]+)?,[[:space:]]+[[:alpha:][:space:]\.-]+$'
+regex = '^((de(l?|([[:space:]]+la))|san)[[:space:]])?[[:alpha:]-]+([[:space:]]+((de(l?|([[:space:]]+la))|san)[[:space:]])?[[:alpha:]-]+)?[[:space:]]*,[[:space:]]+[[:alpha:][:space:]\.-]+$'
 
 cursor.execute("select count(*) from person where name like '%,%';")
 total_persons_with_comma = cursor.fetchall()[0][0]
@@ -23,10 +23,10 @@ total_persons_matched = cursor.fetchall()[0][0]
 
 #print 'Distinct persons: %.2f' % (total_distinct_persons / float(total_persons) * 100)
 
-missing_query = "select count(*) from person where name like '%%,%%' and lower(name) not regexp '%s';" % regex
-cursor.execute(missing_query)
-print missing_query
+cursor.execute("select count(*) from person where name like '%%,%%' and lower(name) not regexp '%s';" % regex)
 total_missing_persons = cursor.fetchall()[0][0]
+
+print "select name from person where name like '%%,%%' and lower(name) not regexp '%s' limit 10;" % regex
 
 print 'Persons with comma: %.2f' % (total_persons_with_comma / float(total_persons) * 100)
 print '\t Matching: %.2f' % (total_persons_matched / float(total_persons_with_comma) * 100)
