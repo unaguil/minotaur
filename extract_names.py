ef3c13a5dc8eb115c7e655f376c36153ed55d936
Unai Aguilera <unai.aguilera@deusto.es>
2013-10-17 13:48:37 +0200

    Matching statistics printed

--- extract_names.py
+++ extract_names.py
@@ -9,14 +9,17 @@ database='teseo'
 conn = MySQLdb.Connection(db=database, host=host, user=user, passwd=password)
 cursor = conn.cursor()
 
-sql_regex = '^([[:space:]]*((de(l?|([[:space:]]+la))|san|la)[[:space:]]+)?[[:alpha:]-]+){1,2}[[:space:]]*,?[[:space:]]+[[:alpha:][:space:]\.-]+$'
+cursor.execute('select count(*) from person;')
+total_persons = cursor.fetchall()[0][0]
 
+sql_regex = '^([[:space:]]*((de(l?|([[:space:]]+la))|san|la)[[:space:]]+)?[[:alpha:]-]+){1,2}[[:space:]]*,?[[:space:]]+[[:alpha:][:space:]\.-]+$'
 pattern = re.compile('^(\s*(?:(?:de(?:l?|(?:\s+la))|san|la)\s+)?[\w-]+)(\s*(?:(?:de(?:l?|(?:\s+la))|san|la)\s+)?[\w-]+)?\s*,?\s+([\w\s\.-]+)$', re.UNICODE)
 
 cursor.execute("select id, lower(name) from person where lower(name) regexp '%s';" % sql_regex)
-
 result = cursor.fetchall()
 total = len(result)
+
+missing = 0
 for (index, (id, name)) in enumerate(result):    
     match = re.search(pattern, name)
     if match:
@@ -34,6 +37,9 @@ for (index, (id, name)) in enumerate(result):
             first_name = match.group(3).strip()
         else:
             first_name = ''
+            
+        if first_surname is '' and second_surname is '' and first_name is '':
+            missing = missing + 1
         
         cursor.execute("update person set first_name='%s', first_surname='%s', second_surname='%s' where id=%s;" % (first_name, first_surname, second_surname, id))
 
@@ -45,5 +51,8 @@ sys.stdout.write('\n')
 conn.commit()
 
 print 'Process finished'
+print 'Not matching: %.2f' % (missing / float(total) * 100)
+print ''
+print 'Total missing: %.2f' % ((total_persons - total)/ float(total_persons) * 100)
 
 conn.close()
