bb4147124ea84fb0af6bd82d6c8d84d6d7ed1493
Unai Aguilera <unai.aguilera@deusto.es>
2014-01-13 08:30:49 +0100

    Web skeleton started

--- minotaur.py
+++ minotaur.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask, render_template
 
 from teseo_model import Thesis, Person, Descriptor, Department
@@ -5,7 +7,9 @@ from teseo_model import University, Advisor, PanelMember
 from teseo_model import Base
 
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, extract, func
+
+import json
 
 USER = 'teseo'
 PASS = 'teseo'
@@ -18,14 +22,25 @@ session = Session()
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
 @app.route("/university/<university_id>/progression")
 def show_progression(university_id):
+	
 	university = session.query(University).filter_by(id=university_id).first()
-	return render_template('progression.html', university_id=university_id, university_name=university.name)
+	values = session.query(Thesis.defense_date, func.count(Thesis.id)).filter_by(university=university).group_by(extract('year', Thesis.defense_date))
+
+	data = {}
+	data['key'] = university.name
+
+	rows = []
+	for v in values:
+		row = {}
+		row['label'] = v[0].year
+		row['value'] = v[1]
+		rows.append(row)
+
+	data['values'] = rows  
+
+	return render_template('progression.html', university=university, data=json.dumps(data))
 
 if __name__ == "__main__":
     app.run(debug=True)
